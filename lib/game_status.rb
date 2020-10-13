3845bf749593e33cf0255026e3eb9ddcced2b02a
Ryan Kendig <ryan.kendig@gmail.com>
2020-10-13 19:10:19 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,9 +17,9 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each {|win_combo|
-  index[0] = win_combo[0]
-  index[1] = win_combo[1]
-  index[2] = win_combo[2]
+  index_0 = win_combo[0]
+  index_1 = win_combo[1]
+  index_2 = win_combo[2]
   
   position_1 = board[index_0]
   position_2 = board[index_1]
